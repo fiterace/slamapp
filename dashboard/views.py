deb6115815435c178d630737a24df8ed1e462890
fiterace <namantayal.noida@gmail.com>
2020-07-07 19:34:24 +0530

    fixed spelling error

--- dashboard/views.py
+++ dashboard/views.py
@@ -89,7 +89,7 @@ def fillSlambook_PageView(request,pk):
     return render(request,'dashboard/fillSlambook.html',{'form':form, "is_senior":False})
 
 class showSlambooksAll(LoginRequiredMixin,ListView):
-    template_name='dashboard/showSlamBooks_all.html'
+    template_name='dashboard/showSlambooks_all.html'
     colors = ["#CFD0FF", "#FFF3C3", "#FECCCB", "#A3E9C6", "#a6dcef", "#9aceff", "#a7e9af", "#a0ffe6", "#d5fffd", "#ffa5b0", "#f6def6", "#CFD0FF", "#FFF3C3", "#FECCCB", "#A3E9C6", "#a6dcef", "#9aceff", "#a7e9af", "#a0ffe6", "#d5fffd", "#ffa5b0", "#f6def6", "#f2aaaa", "#dbc6eb", "#bbf1cb", "#abc2e8", "b4f2e1"] 
     queryset = list(zip(senior_users.objects.all(), colors))
     context_object_name = 'zip'
@@ -104,7 +104,7 @@ class showSlambooksAll(LoginRequiredMixin,ListView):
 
 
 class showSlambooksMyListView(LoginRequiredMixin,ListView, UserPassesTestMixin):
-    template_name='dashboard/showSlamBooks_my.html'
+    template_name='dashboard/showSlambooks_my.html'
     colors = ["#CFD0FF", "#FFF3C3", "#FECCCB", "#A3E9C6", "#a6dcef", "#9aceff", "#a7e9af", "#a0ffe6", "#d5fffd", "#ffa5b0", "#f6def6", "#CFD0FF", "#FFF3C3", "#FECCCB", "#A3E9C6", "#a6dcef", "#9aceff", "#a7e9af", "#a0ffe6", "#d5fffd", "#ffa5b0", "#f6def6"] 
     queryset = list(zip(tableThree.objects.all(), colors))
     context_object_name = 'zip'
